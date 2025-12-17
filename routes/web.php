66fc6e374ef105a3018e5293ed45616ee965f03d
Bagas-bwabwa <bagas24ti@mahasiswa.pcr.ac.id>
2025-12-17 18:33:29 +0700

    First Commit
    First Commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/', function () {
     return view('welcome');
+})->name('home');
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
 });
+
+Route::get('/register', function () {
+    return view('auth.register');
+})->name('register');
