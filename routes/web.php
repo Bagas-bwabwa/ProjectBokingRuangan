ef45dd01e0eccdcf6a206a9cecace091767f1b65
Bagas-bwabwa <bagas24ti@mahasiswa.pcr.ac.id>
2025-12-18 16:57:36 +0700

    ke tiga
    ke tiga

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Auth\LoginController;
 
 
 // Route::get('/', function () {
@@ -20,3 +21,21 @@ Route::get('/dashboard', function () {
 Route::get('/register', function () {
     return view('auth.register');
 })->name('register');
+
+Route::get('/login', [LoginController::class, 'showLoginForm'])
+    ->name('login');
+
+// proses login
+Route::post('/login', [LoginController::class, 'login']);
+
+// logout
+Route::post('/logout', [LoginController::class, 'logout'])
+    ->name('logout');
+
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+
+
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
